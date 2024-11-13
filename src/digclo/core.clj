7ef0d8c08aa9fe96e06d9a3e3d6c426ea7078d7b
Justin Bishop <mail@dissoc.me>
2024-11-13 16:57:08 -0600

    initial commit

---
+++ src/digclo/core.clj
@@ -0,0 +1,6 @@
+(ns digclo.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/digclo/core_test.clj
@@ -0,0 +1,7 @@
+(ns digclo.core-test
+  (:require [clojure.test :refer :all]
+            [digclo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
