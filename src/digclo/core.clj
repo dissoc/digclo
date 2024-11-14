5cb6c4a92e3206a39606ca3ff30bfd5a63c9dc1f
Justin Bishop <mail@dissoc.me>
2024-11-13 21:10:41 -0600

    basic ip lookup

--- src/digclo/core.clj
+++ src/digclo/core.clj
@@ -1,6 +1,21 @@
-(ns digclo.core)
+(ns digclo.core
+  (:import [java.net Inet6Address Inet4Address InetAddress]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; TODO re-wite for async operation if this is used
+(defn name->ip
+  [name & {:keys [cn? ipv6?]
+           :or   {cn?   false
+                  ipv6? true}}]
+  (loop [result []
+         ips    (InetAddress/getAllByName name)]
+    (let [obj     (first ips)
+          version (cond
+                    (instance? java.net.Inet6Address obj) :ipv6
+                    (instance? java.net.Inet4Address obj) :ip4)
+          ip-map  (cond-> {:ip (.getHostAddress obj)
+                           :version version}
+                    cn? (assoc :cn (.getCanonicalHostName obj)))]
+      (if (next ips)
+        (recur (conj result ip-map)
+               (next ips))
+        (conj result ip-map)))))
