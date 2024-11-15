d722767d542f5689f8bff9a80c16e44a1eea05ae
Justin Bishop <mail@dissoc.me>
2024-11-15 01:30:22 -0600

    basic functionality

--- src/digclo/core.clj
+++ src/digclo/core.clj
@@ -1,15 +1,19 @@
 (ns digclo.core
   (:require
+   [clojure.java.data :as j]
    [clojure.string :refer [ends-with?]]
-   [digclo.utils :refer [record-type-kw->id
-                         record-type-id->kw]])
+   [digclo.utils :refer [record-type-id->kw record-type-kw->id]])
   (:import
    (java.net Inet4Address Inet6Address InetAddress)
    (java.util.function BiConsumer)
-   (org.xbill.DNS DClass Message Name Record Section SimpleResolver)))
+   (org.xbill.DNS ARecord DClass EmptyRecord Message Name OPTRecord Record Section SimpleResolver SOARecord)))
+
+;; useful links
+;; https://www.ietf.org/rfc/rfc1034.txt
 
 ;; TODO re-wite for async operation if this is used
-(defn basic-name->ip
+(defn ^:deprecated basic-name->ip
+  "pure java lookup"
   [name & {:keys [cn? ipv6?]
            :or   {cn?   false
                   ipv6? true}}]
@@ -27,42 +31,61 @@
                (next ips))
         (conj result ip-map)))))
 
-(defn format-a-record [a-record]
-  {:address         (-> a-record
-                        .getAddress
-                        .getHostAddress)
-   :ttl             (-> a-record
-                        .getTTL)
-   :record-type     (-> a-record
-                        .getType
-                        record-type-id->kw)
-   :additional-name (-> a-record
-                        .getAdditionalName)})
+(defn format-record [obj]
+  {:name            (-> obj .getName str)
+   :type            (-> obj .getType record-type-id->kw)
+   :ttl             (-> obj .getTTL)
+   :rr-set-type     (-> obj .getRRsetType)
+   :additional-name (-> obj .getAdditionalName)})
 
-(defn format-question [question]
-  {:name        (-> question .getName str)
-   :record-type (-> question
-                    .getType
-                    record-type-id->kw)
-   :ttl         (-> question .getTTL)})
+(defmethod j/from-java OPTRecord [obj]
+  (merge (format-record obj)
+         {:payload-size    (.getPayloadSize obj)
+          :extended-r-code (-> obj .getExtendedRcode)
+          :options         (-> obj .getOptions)
+          :version         (-> obj .getVersion)
+          :flags           (-> obj .getFlags)}))
 
-(defn format-response [response]
-  {:question   (map format-question
-                    (.getSectionArray response Section/QUESTION))
-   :answer     (map format-a-record
-                    (.getSectionArray response Section/ANSWER))
-   :additional (.getSectionRRsets response Section/ADDITIONAL)
-   :zone       (.getSectionRRsets response Section/ZONE)})
+(defmethod j/from-java ARecord [obj]
+  (merge (format-record obj)
+         {:address (-> obj .getAddress .getHostAddress)}))
 
-(defn lookup [name & {:keys [server record-type on-answer]
+(defmethod j/from-java EmptyRecord [obj]
+  (format-record obj))
+
+(defmethod j/from-java SOARecord [obj]
+  (merge (format-record obj)
+         {:host    (-> obj .getHost j/from-java)
+          :admin   (-> obj .getAdmin j/from-java)
+          :serial  (-> obj .getSerial)
+          :refresh (-> obj .getRefresh)
+          :retry   (-> obj .getRetry)
+          :expire  (-> obj .getExpire)
+          :minimum (-> obj .getMinimum)}))
+
+(defmethod j/from-java Name [obj]
+  (str obj))
+
+(defmethod j/from-java Message [message]
+  {:question   (mapv j/from-java
+                     (.getSectionArray message Section/QUESTION))
+   :answer     (mapv j/from-java
+                     (.getSectionArray message Section/ANSWER))
+   :authority  (mapv j/from-java
+                     (.getSectionArray message Section/AUTHORITY))
+   :additional (mapv j/from-java
+                     (.getSectionArray message Section/ADDITIONAL))})
+
+(defn lookup [name & {:keys [server record-type on-answer raw?]
                       :or   {server      "8.8.8.8"
+                             raw?        false
                              record-type :a}}]
   (let [n             (Name/fromString (if (ends-with? name ".")
                                          name
                                          (str name ".")))
         r             (new SimpleResolver server)
         query-record  (Record/newRecord n
-                                        (record-type-kw->id :a)
+                                        (record-type-kw->id record-type)
                                         DClass/IN)
         query-message (Message/newQuery query-record)
         answer        (.sendAsync r query-message)]
@@ -70,5 +93,9 @@
       (.whenComplete answer (reify
                               BiConsumer
                               (accept [_ m e]
-                                (on-answer m))))
-      (format-response @answer))))
+                                (on-answer (if raw?
+                                             (str m)
+                                             (j/from-java m))))))
+      (if raw?
+        (str @answer)
+        (j/from-java @answer)))))
